ea14faae6f5e1bec25df04f232ea4bbeeaae54a7
Harris Mazhar <101860888+Harris-dev26@users.noreply.github.com>
2022-12-10 08:29:34 +0500

    Update settings.py

--- touristfront/settings.py
+++ touristfront/settings.py
@@ -86,15 +86,16 @@ WSGI_APPLICATION = 'touristfront.wsgi.application'
 #     }
 # }
 
+
+
 DATABASES = {
 
     "default": {
         
         "ENGINE": "django.db.backends.sqlite3",
         "NAME":  os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+    }
 }
-
 
 
 
